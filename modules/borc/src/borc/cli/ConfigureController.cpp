99ac968d5b8e2f32d30fd0d4e3d66fc8acdf949f
Felipe Apablaza <ing.apablaza@gmail.com>
2019-07-29 22:52:38 -0400

    Imported build-related sources from original Borc project

--- modules/borc/src/borc/cli/ConfigureController.cpp
+++ modules/borc/src/borc/cli/ConfigureController.cpp
@@ -71,10 +71,9 @@ namespace borc {
         };
         */
 
-        // *** 
         std::unique_ptr<Package> package = this->makePackage(packageEntity, moduleEntities);
 
-        // Now we need to 
+        // Now we need to start the build!
 
         // Now we have parsed all the artifacts in the main package. 
         // Let's parse all the additional packages. We need that when we solve all the dependencies to create references
