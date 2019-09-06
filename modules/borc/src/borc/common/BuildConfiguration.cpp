5c5ed449d5aadc04d1d0a41f0f7b02b9aeed01ae
Felipe Apablaza <ing.apablaza@gmail.com>
2019-09-05 23:03:17 -0400

    Added initial BuildConfiguration struct

---
+++ modules/borc/src/borc/common/BuildConfiguration.cpp
@@ -0,0 +1,6 @@
+
+#include "BuildConfiguration.hpp"
+
+namespace borc {
+    
+}
---
+++ modules/borc/src/borc/common/BuildConfiguration.hpp
@@ -0,0 +1,22 @@
+
+#ifndef __BORC_COMMON_BUILDCONFIGURATION_HPP__
+#define __BORC_COMMON_BUILDCONFIGURATION_HPP__
+
+#include <vector>
+#include <utility>
+#include <borc/utility/MetaProperty.hpp>
+#include <borc/model/Version.hpp>
+
+namespace borc {
+    struct BuildConfiguration {
+        std::string toolchain;
+        Version version;
+
+        constexpr static auto properties = std::make_tuple (
+            property(&BuildConfiguration::toolchain, "toolchain"),
+            property(&BuildConfiguration::version, "version")
+        );
+    };
+}
+
+#endif
