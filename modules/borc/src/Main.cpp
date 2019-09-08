42190631dffb91dc3e4902db31a6866a789e9b9b
Felipe Apablaza <ing.apablaza@gmail.com>
2019-09-07 22:17:49 -0400

    Added boost::graph POC

--- modules/borc/src/Main.cpp
+++ modules/borc/src/Main.cpp
@@ -32,10 +32,38 @@ int main(int argc, char **argv) {
 */
 
 #include <iostream>
-#include <boost/filesystem.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+
+struct VertexProperties {
+    std::string name;
+};
+
+boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS, VertexProperties> createEmptyDirectedGraph() noexcept {
+    return {};
+}
 
 int main(int argc, char **argv) {
-    
+    // Un vertice se maneja con un Handle
+    auto directedGraph = createEmptyDirectedGraph();
+    auto v1 = boost::add_vertex(directedGraph);
+    auto v2 = boost::add_vertex(directedGraph);
+    auto v3 = boost::add_vertex(directedGraph);
+    auto v4 = boost::add_vertex(directedGraph);
+    auto v5 = boost::add_vertex(directedGraph);
+    auto v6 = boost::add_vertex(directedGraph);
+
+    directedGraph[v1].name = "Hola";
+
+    auto edgeResult1 = boost::add_edge(v1, v2, directedGraph);
+    auto edgeResult2 = boost::add_edge(v2, v3, directedGraph);
+    auto edgeResult3 = boost::add_edge(v1, v3, directedGraph);
+    auto edgeResult4 = boost::add_edge(v3, v4, directedGraph);
+    auto edgeResult5 = boost::add_edge(v4, v5, directedGraph);
+    auto edgeResult6 = boost::add_edge(v5, v6, directedGraph);
+    auto edgeResult7 = boost::add_edge(v3, v6, directedGraph);
+
+    boost::write_graphviz(std::cout, directedGraph);
 
     return 0;
 }
