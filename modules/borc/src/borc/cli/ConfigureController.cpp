07ca460b008782d230a74fb0a92e2180054858f9
Felipe Apablaza <ing.apablaza@gmail.com>
2019-10-05 14:41:24 -0300

    Added bare-bones configure implementation

--- modules/borc/src/borc/cli/ConfigureController.cpp
+++ modules/borc/src/borc/cli/ConfigureController.cpp
@@ -1,10 +1,21 @@
 
 #include "ConfigureController.hpp"
+#include <iostream>
 
 namespace borc {
     ConfigureController::~ConfigureController() {}
 
     void ConfigureController::perform(const ConfigureControllerOptions &options) {
-        
+        if (options.showHelp) {
+            std::cout << options.helpMessage;
+
+            return;
+        }
+
+        if (!options.buildType && !options.toolchain) {
+            throw std::runtime_error("Must select a build type and a toolchain.");
+        }
+
+        std::cout << "Configuring build: type=" << options.buildType.get() << ", toolchain=" << options.toolchain.get() << std::endl;
     }
 }
