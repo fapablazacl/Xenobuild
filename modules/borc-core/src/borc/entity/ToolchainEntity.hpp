fc1a952e9c5fa180051cdd02c12dea53dd6afd2d
Felipe Apablaza <ing.apablaza@gmail.com>
2019-11-17 19:53:17 -0300

    Added initial ToolchainEntity definition

---
+++ modules/borc-core/src/borc/entity/ToolchainEntity.hpp
@@ -0,0 +1,62 @@
+
+#ifndef __BORC_ENTITY_TOOLCHAIN_HPP__
+#define __BORC_ENTITY_TOOLCHAIN_HPP__
+
+#include <string>
+#include <vector>
+
+#include "LanguageEntity.hpp"
+#include "ModuleEntity.hpp"
+
+#include <boost/hana.hpp>
+
+namespace borc {
+    struct ToolchainEntity {
+        struct Switches {
+            /* compiler switches */
+            std::string debugInformation;
+            std::string includePath;
+            std::string compile;
+            std::string outputFile;
+            std::string generateBuildDependencies;
+
+            /* linker switches */
+            std::string outputFile;
+            std::string importLibrary;
+            std::string libraryPath;
+        };
+
+        struct BuildRuleInput {
+            std::string fileType;
+        };
+
+        struct BuildRuleOutput {
+            std::string fileType;
+            std::string fileName;
+        };
+
+        struct BuildRule {
+            BuildRuleInput input;
+            std::vector<std::string> flags;
+            BuildRuleOutput output;
+        };
+
+        struct Tool {
+            std::string type;
+            std::string command;
+
+            std::vector<BuildRule> buildRules;
+
+            Switches switches;
+        };
+
+        std::string name;
+        std::vector<Tool> tools;
+
+        typedef void DefaultType;
+    };
+}
+
+// BOOST_HANA_ADAPT_STRUCT(borc::PackageEntity, name, description, language, modules, packageSearchPaths);
+
+#endif
