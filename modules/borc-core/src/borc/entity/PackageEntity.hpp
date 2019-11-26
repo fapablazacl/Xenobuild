59b7079925b2e13e178cd339a9c149f4a7d020e3
Felipe Apablaza <ing.apablaza@gmail.com>
2019-11-26 20:37:48 -0300

    Added new fields in PackageEntity

--- modules/borc-core/src/borc/entity/PackageEntity.hpp
+++ modules/borc-core/src/borc/entity/PackageEntity.hpp
@@ -12,16 +12,41 @@
 
 namespace borc {
     struct PackageEntity {
+        struct Required {
+            std::string name;
+            std::string type;
+            std::string desc;
+
+            typedef void DefaultType;
+        };
+
+        struct Module {
+            bool common;
+            bool prebuilt;
+            std::string includePath;
+            std::string libraryPath;
+
+            std::string name;
+            std::string library;
+
+            typedef void DefaultType;
+        };
+
         std::string name;
         std::string description;
+        std::vector<Required> required;
         LanguageEntity language;
         std::vector<std::string> modulePaths;
         std::vector<std::string> packageSearchPaths;
+
+        std::vector<Module> modules;
 
         typedef void DefaultType;
     };
 }
 
-BOOST_HANA_ADAPT_STRUCT(borc::PackageEntity, name, description, language, modulePaths, packageSearchPaths);
+BOOST_HANA_ADAPT_STRUCT(borc::PackageEntity::Required, name, type, desc);
+BOOST_HANA_ADAPT_STRUCT(borc::PackageEntity::Module, common, prebuilt, includePath, libraryPath, name, library);
+BOOST_HANA_ADAPT_STRUCT(borc::PackageEntity, name, description, required, language, modulePaths, packageSearchPaths, modules);
 
 #endif
