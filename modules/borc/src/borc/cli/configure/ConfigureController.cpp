8186b0e75793cd56208528e6b3b0ea9cfb44a0e4
Felipe Apablaza <ing.apablaza@gmail.com>
2019-11-26 20:31:18 -0300

    Fixed stock package search path problem

--- modules/borc/src/borc/cli/configure/ConfigureController.cpp
+++ modules/borc/src/borc/cli/configure/ConfigureController.cpp
@@ -150,10 +150,15 @@ namespace borc {
     std::unique_ptr<PackageRegistry> ConfigureController::createPackageRegistry(PackageService *packageService, const boost::filesystem::path &packageRegistryPath) const {
         using boost::filesystem::directory_entry;
         using boost::filesystem::directory_iterator;
+        using boost::filesystem::is_directory;
 
         auto packageRegistry = std::make_unique<PackageRegistry>();
 
         for (directory_entry &entry : directory_iterator{packageRegistryPath}) {
+            if (! is_directory(entry.path())) {
+                continue;
+            }
+
             auto package = packageService->createPackage(entry.path(), nullptr);
 
             packageRegistry->registerPackage(std::move(package));
