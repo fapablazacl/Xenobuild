c44707c2a0b83e9bf19d26a35644d765968c4bf3
Felipe Apablaza <ing.apablaza@gmail.com>
2020-02-06 13:31:18 -0300

    Fixed dev issue with the Component class

--- component/core/borc/core/package/Component.cpp
+++ component/core/borc/core/package/Component.cpp
@@ -8,21 +8,25 @@ namespace borc {
     Component::~Component() {}
 
     Component* Component::setName(const std::string &value) {
+        name = value;
 
         return this;
     }
 
     Component* Component::setDescription(const std::string &value) {
+        description = value;
 
         return this;
     }
 
     Component* Component::setType(const Component::Type &value) {
+        type = value;
 
         return this;
     }
 
     Component* Component::setFiles(const std::vector<std::string> &values) {
+        files = values;
 
         return this;
     }
