5aca41a69d8e368921b33062ea93a38c01e40741
Felipe Apablaza <ing.apablaza@gmail.com>
2020-02-06 16:55:03 -0300

    Added Pipe class implementation

--- component/core/borc/core/pipeline/Pipe.cpp
+++ component/core/borc/core/pipeline/Pipe.cpp
@@ -2,5 +2,66 @@
 #include "Pipe.hpp"
 
 namespace borc {
+    Pipe::Pipe(const Pipeline *pipeline_, const std::string &toolName_) 
+        : pipeline(pipeline_), toolName(toolName_) {}
+
+
     Pipe::~Pipe() {}
+
+
+    void Pipe::setInputPin(const std::string &inputPinName, const Pipe::InputPin &inputPin) {
+        inputPins[inputPinName] = inputPin;
+    }
+
+
+    void Pipe::setInputArg(const std::string &inputArgName, const Pipe::Argument &inputArg) {
+        arguments[inputArgName] = inputArg;
+    }
+
+
+    void Pipe::setOutputPin(const std::string &outputPinName, const Pipe::OutputPin &outputPin) {
+        outputPins[outputPinName] = outputPin;
+    }
+
+
+    std::optional<Pipe::InputPin> Pipe::getInputPin(const std::string &name) const {
+        if (auto it = inputPins.find(name); it != inputPins.end()) {
+            return it->second;
+        }
+
+        return {};
+    }
+
+
+    std::optional<Pipe::Argument> Pipe::getInputArg(const std::string &name) const {
+        if (auto it = arguments.find(name); it != arguments.end()) {
+            return it->second;
+        }
+
+        return {};
+    }
+
+
+    std::optional<Pipe::OutputPin> Pipe::getOutputPin(const std::string &name) const {
+        if (auto it = outputPins.find(name); it != outputPins.end()) {
+            return it->second;
+        }
+
+        return {};
+    }
+
+    
+    int Pipe::getInputPinCount() const {
+        return static_cast<int>(inputPins.size());
+    }
+
+
+    int Pipe::getInputArgCount() const {
+        return static_cast<int>(arguments.size());
+    }
+
+
+    int Pipe::getOutputPinCount() const {
+        return static_cast<int>(outputPins.size());
+    }
 }
--- component/core/borc/core/pipeline/Pipe.hpp
+++ component/core/borc/core/pipeline/Pipe.hpp
@@ -3,46 +3,72 @@
 
 #include <string>
 #include <vector>
+#include <map>
+#include <optional>
 
 namespace borc {
     class Matcher;
 
+    class Pipeline;
     class Pipe {
     public:
+        explicit Pipe(const Pipeline *pipeline, const std::string &toolName);
+
         ~Pipe();
 
         enum class Cardinality {
-            one, many
+            One, Many
         };
 
         struct InputPin {
-            std::string name;
             std::string toolArgumentName;
             Cardinality cardinality;
             Matcher *matcher = nullptr;
         };
 
         struct OutputPin {
-            std::string name;
             Cardinality cardinality;
         };
 
         enum class DataType {
-            _void, string, integer, _float, path, file
+            Void, String, Integer, Float, Folder, File
         };
 
         struct Argument {
-            std::string name;
             Cardinality cardinality;
             std::string toolArgumentName;
             DataType dataType;
         };
 
-
     public:
+        std::string getToolName() const {
+            return toolName;
+        }
+
+        void setInputPin(const std::string &inputPinName, const InputPin &inputPin);
+
+        void setInputArg(const std::string &inputArgName, const Argument &inputArg);
+
+        void setOutputPin(const std::string &outputPinName, const OutputPin &outputPin);
+
+        std::optional<InputPin> getInputPin(const std::string &name) const;
+
+        std::optional<Argument> getInputArg(const std::string &name) const;
+
+        std::optional<OutputPin> getOutputPin(const std::string &name) const;
+
+        int getInputPinCount() const;
+
+        int getInputArgCount() const;
+
+        int getOutputPinCount() const;
+
+    private:
+        const Pipeline *pipeline = nullptr;
+
         std::string toolName;
-        std::vector<InputPin> inputPins;
-        std::vector<Argument> arguments;
-        std::vector<OutputPin> outputPins;
+        std::map<std::string, InputPin> inputPins;
+        std::map<std::string, Argument> arguments;
+        std::map<std::string, OutputPin> outputPins;
     };
 }
